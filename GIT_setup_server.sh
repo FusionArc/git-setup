02a49d545eec026bad7bfc4765ce8093a043f302
David Barrow <dave@fusion-arc.uk>
2020-05-05 06:51:39 +0100

    update script

--- GIT_setup_server.sh
+++ GIT_setup_server.sh
@@ -1,15 +1,19 @@
-!# /bin/bash
+#! /bin/bash
+# Set env vars for script
 export PROJECTSLUG="cloudshare"
 export GIT_SLUG="${PROJECTSLUG}.git"
 export GIT_REPO="${HOME}/${PROJECTSLUG}/repo/${GIT_SLUG}/"
 export GIT_WORK_TREE="${HOME}/${PROJECTSLUG}/src/"
-export REMOTE="https://github.com/FusionArc/${PROJECTSLUG}"
+export REMOTE="https://github.com/${GIT_USER}/${PROJECTSLUG}"
 export PATH=${HOME}/.local/bin:$PATH
 echo 'Creating working directory and git repo...'
-sudo chown -R $HOME $USER
+# Make the required directories
+sudo chown -R $USER $HOME
 mkdir -p $GIT_WORK_TREE
-mkdir -p $GIT_REPO 
+mkdir -p $GIT_REPO
+# install virtualenv
 sudo apt install python3-virtualenv
+# Create a gitconfig file in home directory
 cat <<GIT >> ${HOME}/.gitconfig
 [user]
 	name = David Barrow
@@ -35,7 +39,7 @@ cat <<GIT >> ${HOME}/.gitconfig
 	untracked = red bold
 GIT
 ## For new project
-# git --work-tree=$GIT_WORK_TREE --git-dir=$GIT_REPO clone $REMOTE--bare
+# git --work-tree=${GIT_WORK_TREE} --git-dir=${GIT_REPO} clone ${REMOTE} --bare
 
 ### For existing project 
 cd $GIT_REPO && git clone $REMOTE . --bare
@@ -46,12 +50,11 @@ export VENV_BIN="${HOME}/${PROJECTSLUG}/venv/bin"
 $VENV_BIN/python -m pip install -r $HOME/$PROJECTSLUG/src/requirements.txt
 
 cat <<EOT >> $GIT_REPO/hooks/post-receive
-!#bin/bash
+#! bin/bash
 git --work-tree=${GIT_WORK_TREE} --git-dir=${GIT_REPO} checkout -f
 EOT
 
-chmod +x $HOME/$GIT_REPO/hooks/post-receive
+chmod +x $GIT_REPO/hooks/post-receive
 
-echo "Success. You can access your project on your server at:"
 echo "Be sure to run this on your local repo:"
 echo "git remote add live ssh://${USER}@${PROJECTSLUG}:${GIT_REPO}"
